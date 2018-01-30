18f07840506e9bcbb26bb5dd609528475433faf5
MS KIM <qhshef@gmail.com>
2018-01-30 15:22:54 +0900

    식단표 수정

--- app/src/main/java/com/qhshef/gr/FragFood/FragCamTel.java
+++ app/src/main/java/com/qhshef/gr/FragFood/FragCamTel.java
@@ -126,10 +126,12 @@ public class FragCamTel extends Fragment implements View.OnClickListener {
                 String meal_time = meal.format(now_date);
 
                 // 월, 일 구하기
+                // MM/dd 형식과 M/d 형식으로 구분
                 String[] dates = getTime.split("[/ ]");
                 int getMonth;
                 int getDay;
 
+                // M/d 형식
                 if(dates.length > 1){
                     getMonth = Integer.valueOf(dates[0]);
                     getDay = Integer.valueOf(dates[1]);
@@ -137,6 +139,10 @@ public class FragCamTel extends Fragment implements View.OnClickListener {
                     getMonth = 0;
                     getDay = 0;
                 }
+
+                // MM/dd 형식
+                String getMM = dates[0];
+                String getDD = dates[1];
 
                 // 시간 int형으로 변경
                 int int_meal_time = Integer.parseInt(meal_time);
@@ -160,7 +166,7 @@ public class FragCamTel extends Fragment implements View.OnClickListener {
 
                 // 날짜 전달
                 for(int i=0;i<date.size();i++){
-                    // 오늘 날 찾기
+                    // 오늘 날 찾기( ex) 1/3 )
                     if((getMonth+"/"+getDay+" "+"["+date_of_week+"]").equals(date.get(i).text())){
 
                         // 날짜 설정
@@ -184,6 +190,31 @@ public class FragCamTel extends Fragment implements View.OnClickListener {
                             result.put("main", day.get(i*3-1).text());
                         }
                     }
+                    // 오늘 날 찾기( ex) 01/03 )
+                    else if((getMM+"/"+getDD+" "+"["+date_of_week+"]").equals(date.get(i).text())){
+
+                        // 날짜 설정
+                        result.put("date", date.get(i).text());
+
+                        // 조식 / 메뉴 설정
+                        if(0 <= int_meal_time && int_meal_time < 10){
+                            result.put("meal", date.get(0).text());
+                            result.put("main", day.get(i*3-3).text());
+                        }
+
+                        // 중식 / 메뉴 설정
+                        else if(10 <= int_meal_time && int_meal_time < 14){
+                            result.put("meal", date.get(1).text());
+                            result.put("main", day.get(i*3-2).text());
+                        }
+
+                        // 석식 / 메뉴 설정
+                        else if(14 <= int_meal_time && int_meal_time < 24){
+                            result.put("meal", date.get(2).text());
+                            result.put("main", day.get(i*3-1).text());
+                        }
+                    }
+
                 }
 
                 // 로딩 중
